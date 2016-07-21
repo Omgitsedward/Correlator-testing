525c83db9b7ac30d9eb7101a3c9ae7a94cfa11f3
Omgitsedward <eddietoral16@gmail.com>
2016-07-21 12:13:28 -0700

    Update noise1.py

--- noise1.py
+++ noise1.py
@@ -8,112 +8,120 @@ import corr, struct, numpy as np, matplotlib.pyplot as plt
 s = corr.katcp_wrapper.FpgaClient('10.0.1.217')
 
 s.write_int('shift',2047)
+#Antenna Selection
 s.write_int('antenna_a',0)
-s.write_int('antenna_b',1)
-
-s.write_int('trig',0)
-s.write_int('trig',1)
-s.write_int('trig',0)
-
-s.read_int('overflow')
-s.read_int('address_ac_a')
-s.read_int('address_ac_b')
-s.read_int('address_cc_re')
-s.read_int('address_cc_im')
-s.read_int('address_fft_a')
-s.read_int('address_fft_b')
+s.write_int('antenna_b',4)
 
 f = np.linspace(0,1023,1024)
+accumulation(10000)
 
-k = 0 
-p = 0
+#Adc Data Antenna A
+ad1 = np.asarray(struct.unpack('>1024b',s.read('adc_data1',1024)))
+#Adc Data Antenna B
+ad2 = np.asarray(struct.unpack('>1024b',s.read('adc_data2',1024)))
 
-#Controls number of accumulations with k
-while k < 2000:
-	p = 0
-	s.write_int('trig',0)
-	s.write_int('trig',1)
-	s.write_int('trig',0)
-	while p < 10000:
-		p += 1
-	k += 1
-
-#Cross Correlation of a and b
-ccab = struct.unpack('>2048q',s.read('cc_ab',16384))
-ccab = np.asarray(ccab)
-ccabl = list(ccab)
-temp = []
-i = 0
-while i < 2048:
-	temp.append(ccabl[i] + ccabl[i+1]*1j)
-	i += 2
-	
-cc = np.asarray(temp)
-magcc = abs(cc)
-phasecc = np.angle(cc)
+#Fft Data Antenna A
+fft1 = np.asarray(struct.unpack('>2048l',s.read(fft_data1',8192)))
+fft1l = list(fft1)
+fd1 = np.asarray(splicing(fftl1))
+magfd1 = abs(fd1)
+phasefd1 = np.angle(fd1)*180/np.pi
+#Fft Data Antenna B
+fft2 = np.asarray(struct.unpack('>2048l',s.read(fft_data2',8192)))
+fft2l = list(fft2)
+fd2 = np.asarray(splicing(fftl2))
+magfd2 = abs(fd2)
+phasefd2 = np.angle(fd2)*180/np.pi
 
 #Autocorrelation of A
 aca = struct.unpack('>2048q',s.read('ac_a',16384))
 aca = np.asarray(aca)
 acal = list(aca)
-temp = []
-i = 0
-while i < 2048:
-	temp.append(acal[i] + acal[i+1]*1j)
-	i += 2
-	
-aca = np.asarray(temp)
+aca = np.asarray(splicing(acal))
 magaca = abs(aca)
-phaseaca = np.angle(aca)
+phaseaca = np.angle(aca)*180/np.pi
 
 #Autocorrelation of B
 acb = struct.unpack('>2048q',s.read('ac_b',16384))
 acb = np.asarray(ccab)
 acbl = list(ccab)
-temp = []
-i = 0
-while i < 2048:
-	temp.append(acbl[i] + acbl[i+1]*1j)
-	i += 2
-	
-acb = np.asarray(temp)
+acb = np.asarray(splicing(acbl))
 magacb = abs(acb)
-phaseacb = np.angle(acb)
+phaseacb = np.angle(acb)*180/np.pi
 
+#Cross Correlation of a and b
+ccab = struct.unpack('>2048q',s.read('cc_ab',16384))
+ccab = np.asarray(ccab)
+ccabl = list(ccab)
+cc = np.asarray(splicing(ccabl))
+magcc = abs(cc)
+phasecc = np.angle(cc)*180/np.pi
 
+#Plots of Data
+#Adc Plot
 plt.figure(1)
+plt.title('Adc Data')
+
+plt.subplot(211)
+plt.title('Antenna A')
+plt.plot(f,ad1,'k')
+
+plt.subplot(212)
+plt.title('Antenna B')
+plt.plot(f,ad2,'k')
+plt.show()
+
+#Fft Data plots
+plt.figure(2)
+plt.title('Fft Data')
+
+plt.subplot(411)
+plt.title('Fft of Antenna A')
+plt.plot(f,magfd1,'k')
+plt.subplot(412)
+plt.plot(f,phasefd1,'k')
+
+plt.subplot(413)
+plt.title('Fft of Antenna B')
+plt.plot(f,magfd2,'k')
+plt.subplot(414)
+plt.plot(f,phasefd2,'k')
+plt.show()
+
+#Correlation Plots
+plt.figure(3)
 plt.title('Autocorrelation of Antenna A')
 
 plt.subplot(211)
 plt.title('Magnitude Response of AC of A')
-plt.plot(f,magaca,'b')
+plt.plot(f,magaca,'c')
 plt.ylabel('Power (Arbitrary Units)')
 plt.grid(True)
 
 plt.subplot(212)
 plt.title('Phase Response of AC of A')
-plt.plot(f,phaseaca*180/np.pi,'b')
+plt.plot(f,phaseaca,'c')
 plt.ylabel('Phase in Degrees')
 plt.grid(True)
 plt.show()
 
-plt.figure(2)
+plt.figure(4)
 plt.title('Autocorrelation of Antenna B')
 
 plt.subplot(211)
 plt.title('Magnitude Response of AC of B')
-plt.plot(f,magacb,'g')
+plt.plot(f,magacb,'m')
 plt.ylabel('Power (Arbitrary Units)')
 plt.grid(True)
 
 plt.subplot(212)
 plt.title('Phase Response of AC of B')
-plt.plot(f,phaseacb*180/np.pi,'g')
+plt.plot(f,phaseacb,'m')
 plt.ylabel('Phase in Degrees')
 plt.grid(True)
 plt.show()
 
-plt.figure(3)
+plt.figure(5)
 plt.title('Cross Correlation of Antennas A & B')
 
 plt.subplot(211)
@@ -124,9 +132,28 @@ plt.grid(True)
 
 plt.subplot(212)
 plt.title('Phase Response of CC of A & B')
-plt.plot(f,phasecc*180/np.pi,'r')
+plt.plot(f,phasecc,'r')
 plt.ylabel('Phase in Degrees')
 plt.grid(True)
 plt.show()
 
+#Merges real and imaginary parts of data into a single number
+def splicing(x):
+	temp = []
+	i = 0
+	while i < 2048:
+		temp.append(x[i] + x[i+1]*1j)
+		i += 2
+	return temp
 
+#Controls number of accumulations with n
+def accumulation(n):
+	k = 0
+	while k < n:
+		p = 0
+		s.write_int('trig',0)
+		s.write_int('trig',1)
+		s.write_int('trig',0)
+		while p < 5000:
+			p += 1
+		k += 1
