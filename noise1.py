f40e86c2a1199c943116a71055019bdb9a6f8980
Omgitsedward <eddietoral16@gmail.com>
2016-07-22 14:49:39 -0700

    Update noise1.py

--- noise1.py
+++ noise1.py
@@ -5,9 +5,33 @@
 #8-tap 1024-point polyphase filter bank
 
 import corr, struct, numpy as np, matplotlib.pyplot as plt
-s = corr.katcp_wrapper.FpgaClient('10.0.1.217')
+import time, sys, logging, optparse.OptionParser as p
 
-s.write_int('shift',2047)
+#Merges real and imaginary parts of data into a single number
+def splicing(x):
+	temp = []
+	z = 0
+	while z < 2048:
+		temp.append(x[z] + x[z+1]*1j)
+		z += 2
+	return np.asarray(temp)
+
+#Controls number of accumulations with n
+def accumulation(n):
+	k = 0
+	while k < n:
+		p = 0
+		s.write_int('trig',0)
+		s.write_int('trig',1)
+		s.write_int('trig',0)
+		while p < 8192:
+			p += 1
+		k += 1
+
+
+s = corr.katcp_wrapper.FpgaClient('10.0.1.217')
+s.write_int('shift',961)
+
 #Antenna Selection
 s.write_int('antenna_a',0)
 s.write_int('antenna_b',4)
@@ -17,40 +41,42 @@ accumulation(10000)
 
 #Adc Data Antenna A
 ad1 = np.asarray(struct.unpack('>1024b',s.read('adc_data1',1024)))
+
 #Adc Data Antenna B
 ad2 = np.asarray(struct.unpack('>1024b',s.read('adc_data2',1024)))
 
 #Fft Data Antenna A
 fft1 = np.asarray(struct.unpack('>2048l',s.read('fft_data1',8192)))
 fft1l = list(fft1)
-fd1 = np.asarray(splicing(fftl1))
+fd1 = splicing(fftl1)
 magfd1 = abs(fd1)
 phasefd1 = np.angle(fd1)*180/np.pi
+
 #Fft Data Antenna B
 fft2 = np.asarray(struct.unpack('>2048l',s.read('fft_data2',8192)))
 fft2l = list(fft2)
-fd2 = np.asarray(splicing(fftl2))
+fd2 = splicing(fftl2)
 magfd2 = abs(fd2)
 phasefd2 = np.angle(fd2)*180/np.pi
 
 #Autocorrelation of A
 aca = np.asarray(struct.unpack('>2048q',s.read('ac_a',16384)))
 acal = list(aca)
-aca = np.asarray(splicing(acal))
+aca = splicing(acal)
 magaca = abs(aca)
 phaseaca = np.angle(aca)*180/np.pi
 
 #Autocorrelation of B
 acb = np.asarray(struct.unpack('>2048q',s.read('ac_b',16384)))
 acbl = list(ccab)
-acb = np.asarray(splicing(acbl))
+acb = splicing(acbl)
 magacb = abs(acb)
 phaseacb = np.angle(acb)*180/np.pi
 
 #Cross Correlation of a and b
 ccab = np.asarray(struct.unpack('>2048q',s.read('cc_ab',16384)))
 ccabl = list(ccab)
-cc = np.asarray(splicing(ccabl))
+cc = splicing(ccabl)
 magcc = abs(cc)
 phasecc = np.angle(cc)*180/np.pi
 
@@ -75,12 +101,14 @@ plt.title('Fft Data')
 plt.subplot(411)
 plt.title('Fft of Antenna A')
 plt.plot(f,magfd1,'k')
+
 plt.subplot(412)
 plt.plot(f,phasefd1,'k')
 
 plt.subplot(413)
 plt.title('Fft of Antenna B')
 plt.plot(f,magfd2,'k')
+
 plt.subplot(414)
 plt.plot(f,phasefd2,'k')
 plt.show()
@@ -133,24 +161,3 @@ plt.plot(f,phasecc,'r')
 plt.ylabel('Phase in Degrees')
 plt.grid(True)
 plt.show()
-
-#Merges real and imaginary parts of data into a single number
-def splicing(x):
-	temp = []
-	i = 0
-	while i < 2048:
-		temp.append(x[i] + x[i+1]*1j)
-		i += 2
-	return temp
-
-#Controls number of accumulations with n
-def accumulation(n):
-	k = 0
-	while k < n:
-		p = 0
-		s.write_int('trig',0)
-		s.write_int('trig',1)
-		s.write_int('trig',0)
-		while p < 5000:
-			p += 1
-		k += 1
