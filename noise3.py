403e89cddc31e9ca772b4b55d56e139b4b25821b
Omgitsedward <eddietoral16@gmail.com>
2016-08-15 14:09:52 -0700

    Update noise3.py

--- noise3.py
+++ noise3.py
@@ -1,15 +1,21 @@
-#Testing info:
+#Eddie Toral
+#August 15th 2016
+#12-input Correlator for SNAP Board
+#
+#	Testing info:
 #		Clk Freq = 250 MHz, Input signal freq = 75 Mhz at -6.0 dBm 
-#Noise sources: 
+#	Noise sources: 
 #		Amps in series for one source, L-band generator for the other. 
 #		Couplers used in reverse to combine tone with noise to use for testing.
-#Using 12-input SNAP Board w/ RaspberryPi 
-#10 stage fft_biplex_real_2x
-#4-tap 1024 polyphase filter bank
-#This one will Correlate all 12 inputs at the same time
-#2 input worked (noise3_2016-8-9_1406.bof)
-#4 input 1 ADC worked (noise3_2016-8-9_1501.bof)
-#12 input, 3 ADC works (noise3_2016-8-14_.bof)
+#
+#	Using 12-input SNAP Board w/ RaspberryPi 
+#	10 stage fft_biplex_real_2x
+#	4-tap 1024 polyphase filter bank
+#	Model & code will Correlate all 12 inputs at the same time
+#
+#	2 input worked (noise3_2016-8-9_1406.bof)
+#	4 input 1 ADC worked (noise3_2016-8-9_1501.bof)
+#	12 input, 3 ADC works (noise3_2016-8-14_.bof)
 
 import corr, struct, numpy as np, matplotlib.pyplot as plt, time
 
@@ -41,28 +47,34 @@ s = corr.katcp_wrapper.FpgaClient(host,7147,timeout = 10)
 time.sleep(1)
 
 #--------------------------------------------------------------------------------------------------------------------------------------
+#To make sure RaspberryPi and SNAP Board are connected
 if s.is_connected():
 	print "Connected"
 else:
 	print "Not connected"
 
 #--------------------------------------------------------------------------------------------------------------------------------------
+#To be used for plotting purposes at the end of the code
 fn = np.linspace(0,511,512)
 
 #--------------------------------------------------------------------------------------------------------------------------------------
+#Setting FFT Biplex x2 Shift value
 print "Setting Shift value"
 s.write_int('shift',shift)
 print "Done"
 
 #--------------------------------------------------------------------------------------------------------------------------------------
+#Setting the Accumulation length to option choice selected
 print "Starting accumulation process"
 s.write_int('acc_len',length)
 
 #--------------------------------------------------------------------------------------------------------------------------------------
 #Start Data Processing & Capture
+#Sending Sync Pulse
 s.write_int('trig',0)
 s.write_int('trig',1)
 s.write_int('trig',0)
+
 #Extra accumulations done before reading data from BRAMs for saftey of data
 #It tosses out unknown initial state junk values that would mess up data.
 #Should be working by final accumulation transition to read BRAMs
@@ -87,7 +99,7 @@ overflow = s.read_int('overflow')
 print overflow
 
 #--------------------------------------------------------------------------------------------------------------------------------------
-#Reading Data from BRAM Blocks
+#Reading Data from all Correlation BRAM Blocks
 #Antenna names correspond to the antenna names for the inputs on the SNAP board.
 #		ie. ADC0 Board input -> ac0,a0, etc. in the following lines
 #Cross Correlations will have the naming convention of ccantenna_Aantenna_B
@@ -131,6 +143,7 @@ cc010r = np.asarray(struct.unpack('>512l',s.read('cc_a0_a10_real',2048)))
 cc010i = np.asarray(struct.unpack('>512l',s.read('cc_a0_a10_imag',2048)))
 cc011r = np.asarray(struct.unpack('>512l',s.read('cc_a0_a11_real',2048)))
 cc011i = np.asarray(struct.unpack('>512l',s.read('cc_a0_a11_imag',2048)))
+
 #Antenna 1 Cross 2 - 11
 cc12r = np.asarray(struct.unpack('>512l',s.read('cc_a1_a2_real',2048)))
 cc12i = np.asarray(struct.unpack('>512l',s.read('cc_a1_a2_imag',2048)))
@@ -152,6 +165,7 @@ cc110r = np.asarray(struct.unpack('>512l',s.read('cc_a1_a10_real',2048)))
 cc110i = np.asarray(struct.unpack('>512l',s.read('cc_a1_a10_imag',2048)))
 cc111r = np.asarray(struct.unpack('>512l',s.read('cc_a1_a11_real',2048)))
 cc111i = np.asarray(struct.unpack('>512l',s.read('cc_a1_a11_imag',2048)))
+
 #Atenna 2 Cross 3 - 11
 cc23r = np.asarray(struct.unpack('>512l',s.read('cc_a2_a3_real',2048)))
 cc23i = np.asarray(struct.unpack('>512l',s.read('cc_a2_a3_imag',2048)))
@@ -171,6 +185,7 @@ cc210r = np.asarray(struct.unpack('>512l',s.read('cc_a2_a10_real',2048)))
 cc210i = np.asarray(struct.unpack('>512l',s.read('cc_a2_a10_imag',2048)))
 cc211r = np.asarray(struct.unpack('>512l',s.read('cc_a2_a11_real',2048)))
 cc211i = np.asarray(struct.unpack('>512l',s.read('cc_a2_a11_imag',2048)))
+
 #Antenna 3 Cross 4 - 11
 cc34r = np.asarray(struct.unpack('>512l',s.read('cc_a3_a4_real',2048)))
 cc34i = np.asarray(struct.unpack('>512l',s.read('cc_a3_a4_imag',2048)))
@@ -188,6 +203,7 @@ cc310r = np.asarray(struct.unpack('>512l',s.read('cc_a3_a10_real',2048)))
 cc310i = np.asarray(struct.unpack('>512l',s.read('cc_a3_a10_imag',2048)))
 cc311r = np.asarray(struct.unpack('>512l',s.read('cc_a3_a11_real',2048)))
 cc311i = np.asarray(struct.unpack('>512l',s.read('cc_a3_a11_imag',2048)))
+
 #Antenna 4 Cross 5 - 11
 cc45r = np.asarray(struct.unpack('>512l',s.read('cc_a4_a5_real',2048)))
 cc45i = np.asarray(struct.unpack('>512l',s.read('cc_a4_a5_imag',2048)))
@@ -203,6 +219,7 @@ cc410r = np.asarray(struct.unpack('>512l',s.read('cc_a4_a10_real',2048)))
 cc410i = np.asarray(struct.unpack('>512l',s.read('cc_a4_a10_imag',2048)))
 cc411r = np.asarray(struct.unpack('>512l',s.read('cc_a4_a11_real',2048)))
 cc411i = np.asarray(struct.unpack('>512l',s.read('cc_a4_a11_imag',2048)))
+
 #Antenna 5 Cross 6 - 11
 cc56r = np.asarray(struct.unpack('>512l',s.read('cc_a5_a6_real',2048)))
 cc56i = np.asarray(struct.unpack('>512l',s.read('cc_a5_a6_imag',2048)))
@@ -216,6 +233,7 @@ cc510r = np.asarray(struct.unpack('>512l',s.read('cc_a5_a10_real',2048)))
 cc510i = np.asarray(struct.unpack('>512l',s.read('cc_a5_a10_imag',2048)))
 cc511r = np.asarray(struct.unpack('>512l',s.read('cc_a5_a11_real',2048)))
 cc511i = np.asarray(struct.unpack('>512l',s.read('cc_a5_a11_imag',2048)))
+
 #Antenna 6 Cross 7 - 11
 cc67r = np.asarray(struct.unpack('>512l',s.read('cc_a6_a7_real',2048)))
 cc67i = np.asarray(struct.unpack('>512l',s.read('cc_a6_a7_imag',2048)))
@@ -227,6 +245,7 @@ cc610r = np.asarray(struct.unpack('>512l',s.read('cc_a6_a10_real',2048)))
 cc610i = np.asarray(struct.unpack('>512l',s.read('cc_a6_a10_imag',2048)))
 cc611r = np.asarray(struct.unpack('>512l',s.read('cc_a6_a11_real',2048)))
 cc611i = np.asarray(struct.unpack('>512l',s.read('cc_a6_a11_imag',2048)))
+
 #Antenna 7 Cross 8 - 11
 cc78r = np.asarray(struct.unpack('>512l',s.read('cc_a7_a8_real',2048)))
 cc78i = np.asarray(struct.unpack('>512l',s.read('cc_a7_a8_imag',2048)))
@@ -236,6 +255,7 @@ cc710r = np.asarray(struct.unpack('>512l',s.read('cc_a7_a10_real',2048)))
 cc710i = np.asarray(struct.unpack('>512l',s.read('cc_a7_a10_imag',2048)))
 cc711r = np.asarray(struct.unpack('>512l',s.read('cc_a7_a11_real',2048)))
 cc711i = np.asarray(struct.unpack('>512l',s.read('cc_a7_a11_imag',2048)))
+
 #Antenna 8 Cross 9 - 11
 cc89r = np.asarray(struct.unpack('>512l',s.read('cc_a8_a9_real',2048)))
 cc89i = np.asarray(struct.unpack('>512l',s.read('cc_a8_a9_imag',2048)))
@@ -243,17 +263,23 @@ cc810r = np.asarray(struct.unpack('>512l',s.read('cc_a8_a10_real',2048)))
 cc810i = np.asarray(struct.unpack('>512l',s.read('cc_a8_a10_imag',2048)))
 cc811r = np.asarray(struct.unpack('>512l',s.read('cc_a8_a11_real',2048)))
 cc811i = np.asarray(struct.unpack('>512l',s.read('cc_a8_a11_imag',2048)))
+
 #Antenna 9 Cross 10 - 11
 cc910r = np.asarray(struct.unpack('>512l',s.read('cc_a9_a10_real',2048)))
 cc910i = np.asarray(struct.unpack('>512l',s.read('cc_a9_a10_imag',2048)))
 cc911r = np.asarray(struct.unpack('>512l',s.read('cc_a9_a11_real',2048)))
 cc911i = np.asarray(struct.unpack('>512l',s.read('cc_a9_a11_imag',2048)))
+
 #Antenna 10 Cross 11
 cc1011r = np.asarray(struct.unpack('>512l',s.read('cc_a10_a11_real',2048)))
 cc1011i = np.asarray(struct.unpack('>512l',s.read('cc_a10_a11_imag',2048)))
 
 
 #--------------------------------------------------------------------------------------------------------------------------------------
+#Determination of the Magnitude Responses of the Autocorrelations of each input
+#Phase Responses not solved for to save BRAM Space (no accumulation & storage of imaginary data stream)
+#as Phase Responses are 0 for Autocorrelations
+
 #Autocorrelation of 0
 magac0 = abs(ac0)
 #Autocorrelation of 1
@@ -280,6 +306,9 @@ magac10 = abs(ac10)
 magac11 = abs(ac11)
 
 #--------------------------------------------------------------------------------------------------------------------------------------
+#Recombing real and imaginary parts of the Accumulated Cross Correlations 
+#& Solving for Magnitudes and Phase Responses of the Cross Correlations
+
 #Cross Correlation of 0 and 1
 cc01rl = list(cc01r)
 cc01il = list(cc01i)
