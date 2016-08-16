586f28dc72c07790c817be3fb693a7737922a4d4
Omgitsedward <eddietoral16@gmail.com>
2016-08-16 16:03:55 -0700

    Update adc_stats.py

--- adc_stats.py
+++ adc_stats.py
@@ -1,12 +1,21 @@
-#Simple model used to obtain data to determine analog input level to digital output level relationship.
-#75.0 MHz single tone from input levels +12dBm to -36.0 dBm.
-#Direct connection to input of SNAP Board, no attenuators or filters used during testing.
+#	Eddie Toral
+#	CAMPARE Summer 2016 Research Undergrad
+#	August 16th, 2016
+#	
+#	Simple model used to obtain data to determine analog input level to digital output level relationship.
+#	75.0 MHz single tone from input levels +12dBm to -36.0 dBm.
+#	Direct connection to input of SNAP Board, no attenuators or filters used during testing.
+#	Only 1 antenna at a time, selectable by [option] -a to be selected when running script
 
+from argparse import ArgumentParser
+p = ArgumentParser(description = 'python adc_stats.py [options] ')
+p.add_argument('host', type = str, default = '10.0.1.217', help = 'Specify the host name')
+p.add_argument('-a', '--antenna', dest = 'antenna', type = int, default = 2047, help = 'antenna selection')
 
 import corr, struct, numpy as np, matplotlib.pyplot as plt, time
 s = corr.katcp_wrapper.FpgaClient('10.0.1.217',7147,timeout = 10)
 time.sleep(1)
-
+s.write_int('antenna',antenna)
 s.write_int('cnt_rst',0)
 s.write_int('cnt_rst',1)
 s.write_int('cnt_rst',0)
@@ -27,7 +36,7 @@ print "Hey this one is rms"
 print rms
 
 plt.figure(1)
-plt.title('adc data')
+plt.title('adc data: Antenna %n',antenna)
 plt.plot(time,ad,'k')
 plt.axis([0,65535,-136,135])
 plt.grid(True)
